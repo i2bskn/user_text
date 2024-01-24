fc8c6a37f9d8fa064fd6ac3ea80edd40737626ad
i2bskn <i2bskn@gmail.com>
2024-01-24 21:15:25 +0900

    implement user_text helper

--- lib/user_text.rb
+++ lib/user_text.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/string/output_safety"
+require "rails-html-sanitizer"
+
+require_relative "user_text/helper"
 require_relative "user_text/version"
 
 module UserText
-  class Error < StandardError; end
-  # Your code goes here...
+  extend ::UserText::Helper
 end
